86fe8c6f8ced7db8067ad7c6efc2ed48a7b98ca4
alex <amoralopez@csumb.edu>
2023-12-05 23:02:26 -0800

    commit

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ import requests, json
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
-# api_date_string = recall_initiation_date
 
-# api_date = datetime.strptime(api_date_string, "%Y-%m-%dT%H:%M:%S")
 
 
 API_URL = "https://api.fda.gov/food/enforcement.json"
@@ -23,11 +21,29 @@ def search():
     user_input = request.form.get('input')
     params = {'search': user_input}
     
-    response = requests.get(API_URL, params = params)
+    response = requests.get(API_URL, params=params)
 
-    try:
-        data = response.json()
-        recalls = data.get('results', [])
-        return render_template('results.html', recalls = recalls, user_input = user_input)
-    except:
+    if response.status_code == 200:
+       try:
+            data = response.json()
+            recalls = data.get('results', [])
+            for recall in recalls:
+                datestr = recall['report_date']
+                year = datestr[0:4]
+                month = datestr[5:6]
+                day = datestr[6:8]
+            reportDate = f"{month}/{day}/{year}"
+            for recall in recalls:
+                datestr = recall['recall_initiation_date']
+                year = datestr[0:4]
+                month = datestr[5:6]
+                day = datestr[6:8]
+            recallDate = f"{month}/{day}/{year}"
+            
+            
+                
+            return render_template('results.html', recalls = recalls, user_input = user_input, recallDate = recallDate, reportDate = reportDate)
+       except:
+            pass
+    else:
         return render_template('results.html', no_results = True, user_input = user_input)
